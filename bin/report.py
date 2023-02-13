3ab3013ff1c48d69b9334212af979cf232ac41e0
Usman Rashid <usman@smme.edu.pk>
2023-02-14 12:59:39 +1300

    LAI edge case added to report

--- bin/report.py
+++ bin/report.py
@@ -2,6 +2,7 @@
 
 from report_modules.parsers.busco_parser import parse_busco_folder
 from report_modules.parsers.tidk_parser import parse_tidk_folder
+from report_modules.parsers.lai_parser import parse_lai_folder
 from report_modules.report_printer import ReportPrinter
 
 
@@ -9,6 +10,7 @@ if __name__ == "__main__":
     data_from_tools = {}
     data_from_tools = {**data_from_tools, **parse_busco_folder()}
     data_from_tools = {**data_from_tools, **parse_tidk_folder()}
+    data_from_tools = {**data_from_tools, **parse_lai_folder()}
 
     report_printer = ReportPrinter()
     report_template = report_printer.print_template(data_from_tools)
---
+++ bin/report_modules/parsers/lai_parser.py
@@ -0,0 +1,55 @@
+
+import os
+from pathlib import Path
+import re
+
+class LAIParser:
+    def __init__(self, file_data):
+        self.file_data = file_data
+        self.stats_dict = {}
+
+    def parse_report(self):
+        self.stats_dict["version"] = self.get_lai_version(self.file_data)
+        self.stats_dict["result"] = self.get_lai_results(self.file_data)
+
+        return self.stats_dict
+
+    def get_lai_version(self, data):
+        p = re.compile("### LTR Assembly Index \(LAI\) (.*) ###")
+        result = p.search(data).group(1).strip()
+        return result
+
+    def get_lai_results(self, data):
+        p = re.compile("【Error】(.*)")
+        match_results = p.findall(data)
+        if len(match_results) < 1:
+            return None
+        
+        return ". ".join([m.strip() for m in match_results])
+
+def parse_lai_folder(folder_name = "lai_outputs"):
+
+    dir = os.getcwdb().decode()
+    lai_folder_path = Path(f"{dir}/{folder_name}")
+    list_of_log_files = lai_folder_path.glob("*.LAI.log")
+
+    data = {"LAI": []}
+
+    for file in list_of_log_files:
+        file_data = ""
+        with open(file, "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                file_data += line
+        parser = LAIParser(file_data)
+        file_tokens = re.findall(
+            r"([a-zA-Z0-9_]+).LAI.log",
+            os.path.basename(str(file)),
+        )
+        stats = {
+            "hap": file_tokens[0],
+            **parser.parse_report(),
+        }
+        data["LAI"].append(stats)
+
+    return data
