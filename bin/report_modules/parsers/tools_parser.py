fe3e8953d4595052836c1651c8ece457af32581a
Usman Rashid <usman@smme.edu.pk>
2026-08-28 19:10:40 +1200

    Fixed juicebox.js version parsing and multi-version bug

--- bin/report_modules/parsers/tools_parser.py
+++ bin/report_modules/parsers/tools_parser.py
@@ -6,11 +6,11 @@ from pygments.formatters import HtmlFormatter
 from pygments.lexers import JsonLexer
 
 
-def parse_tools_yaml():
+def parse_tools_yaml(juicebox_js_ver):
     with open("software_versions.yml") as f:
         tools_dict = yaml.safe_load(f)
         formatted_tools_json = highlight_json(
-            json.dumps(format_tools_dict(tools_dict), indent=4)
+            json.dumps(formatted_tools_list(tools_dict, juicebox_js_ver), indent=4)
         )
 
     return tools_dict, formatted_tools_json
@@ -23,11 +23,13 @@ def highlight_json(json_string):
     return highlight(json_string, lexer, formatter)
 
 
-def format_tools_dict(input_dict):
+def formatted_tools_list(input_dict, juicebox_js_ver):
     output_list = []
     for top_level_value in input_dict.values():
         for key, value in top_level_value.items():
             if (key, value) not in output_list:
                 output_list.append((key, value))
 
-    return dict(sorted(output_list, key=lambda x: x[0]))
+    output_list.append(("juicebox.js", juicebox_js_ver))
+
+    return sorted([f"{k}: {v}" for (k, v) in output_list], key=lambda x: x[0])
